aa739ae742dae487687a886d816e883dc4989799
Obioma Emmanuel <78274374+S-cloud-tech@users.noreply.github.com>
2024-01-11 20:57:31 +0100

    updated Items models

---
+++ item/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=225)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=225)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    # qty_supplied = models.IntegerField()
+    # qty_sold = models.IntegerField()
+    is_sold = models.BooleanField(default=False)
+    is_bad = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # price_supplied_at = models.FloatField()
+    # updated_price = models.FloatField()
+
+    def __str__(self):
+        return self.name 
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=225)
+    commpany_name: models.CharField(max_length=255)
+    contact = models.IntegerField()
+    item = models.ForeignKey(Item, related_name='supplier', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
